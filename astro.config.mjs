693fafff5e0026201bafc5596e7f2b5a8e525c6e
lmssieh <122186255+lmssiehdev@users.noreply.github.com>
2024-12-16 01:26:13 +0100

    chore: init

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import cloudflare from '@astrojs/cloudflare';
+
+import tailwind from '@astrojs/tailwind';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  adapter: cloudflare(),
+  integrations: [tailwind()]
+});
