eff3365389328a97cdee49af28ce966978a605f6
lmssieh <122186255+lmssiehdev@users.noreply.github.com>
2024-12-24 23:21:51 +0100

    extract cache to a helper function

--- src/utils/misc.ts
+++ src/utils/misc.ts
@@ -2,5 +2,9 @@ import clsx, { type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-	return twMerge(clsx(inputs));
+  return twMerge(clsx(inputs));
+}
+
+export function setCache(headers: (HeadersInit | undefined) & Headers) {
+  headers.set("Cache-Control", "s-maxage=86400, stale-while-revalidate");
 }
